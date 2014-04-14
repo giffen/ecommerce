d1d914ba4c86fe1377e958a2b46ee9d96320c138
Andy Giffen <giffen@gmail.com>
2014-04-14 14:07:37 -0700

    Broke the settings.py file into base.py and local.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,7 +1,17 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+# needed to have permissionin VirutalEnv
+admin.autodiscover()
+
 urlpatterns = patterns('',
+		(r'^static/(?P<path>.*)$', 'django.views.static.serve',{
+				'document_root': settings.STATIC_ROOT
+			}),
+		(r'^media/(?P<path>.*)$', 'django.views.static.serve',{
+				'document_root': settings.MEDIA_ROOT
+			}),
     # Examples:
     # url(r'^$', 'ecommerce.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
