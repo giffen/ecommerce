d7381678a8396b5f5d52737a0f53890dbd93b54e
Andy Giffen <giffen@gmail.com>
2014-04-14 14:08:56 -0700

    Add settings folder to commit

---
+++ ecommerce/settings/local.py
@@ -0,0 +1,63 @@
+"""
+Django settings for ecommerce project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/dev/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/dev/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'south',
+    'products',
+    'contact',
+)
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Template location
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)),"static","templates"),
+)
+
+if DEBUG:
+    MEDIA_URL = '/media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)),"static","static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)),"static","media")
+    STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)),"static","static"),
+    )
