52341623b4c7f94572c3375975f0aa7285c4c9ea
Andy Giffen <giffen@gmail.com>
2014-04-15 10:09:14 -0700

    Update contact and products views and url files

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, render_to_response, RequestContext, get_object_or_404
 
+from .models import Contact
+from .forms import ContactForm
 # Create your views here.
+
+def contact_us(request):
+	form = ContactForm(request.POST or none)
+	if form.is_valid():
+		save_form = form.save(commit=false)
+		save_form.save()
+
+		return render_to_response('contact/contact_us.html', locals(), context_instance=RequestContext(request))
+
