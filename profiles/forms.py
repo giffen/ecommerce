0510aa54660de81f0f66dd7242e560fb8afdd14f
Andy Giffen <giffen@gmail.com>
2014-04-19 10:29:36 -0700

    Save CC details in DB and Stripe

--- profiles/forms.py
+++ profiles/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import Address
 
 class AddressForm(forms.ModelForm):
+	save_card = forms.BooleanField(initial=True, required=False)
 	class Meta:
 		model = Address
 		fields = ['nickname', 'address1', 'address2', 'city', 'state', 'postal_code', 'country']
